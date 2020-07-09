f1d934fe84fad3e7fe5b6205519ac8e3f8581617
Curtis <curtissmp@gmail.com>
2020-07-09 15:07:48 -0400

    hash map is now vector

--- ICA_Group 2.cpp
+++ ICA_Group 2.cpp
@@ -3,10 +3,13 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 
 class Shape {
     private:
@@ -17,7 +20,7 @@ class Shape {
 
         //Outputs type of shape, virtual function
         string getType() {
-            return type;
+            return this->type;
         }
 
         //Returns id of shape
@@ -59,19 +62,46 @@ class Square : public Shape {
 };
 
 
+class ShapeCache {
+    private:
+        //Hashmap of shapes
+        vector<Shape*> shapeMap;
+
+    public:
+        void getShape() {
+            //Produces error
+            for(int i = 0; i < shapeMap.size(); i++){
+                cout << shapeMap[i]->getType();
+            }
+            
+        }
+
+        void loadCache(){
+            Rectangle rect;
+            Circle circ;
+            Square square;
+
+            rect.type = "Rectangle";
+            circ.type = "Circle";
+            square.type = "Square";
+            
+           shapeMap.push_back(&rect);
+           shapeMap.push_back(&circ);
+           shapeMap.push_back(&square);
+
+            
+           // cout << shapeMap[0] << endl;
+        }
+};
+
 int main() {
+    
+    //Object with hashmap of shapes
+    ShapeCache shapeCache;
 
-    Rectangle rect;
-    Circle circ;
-    Square square;
-
-    rect.type = "Rectangle";
-    circ.type = "Circle";
-    square.type = "Square";
-
-    cout << rect.getType() << endl;
-    cout << circ.getType() << endl;
-    cout << square.getType() << endl;
+    //Loads the cache with arbitrary shapes
+    shapeCache.loadCache();
+    shapeCache.getShape();
 
     return EXIT_SUCCESS;
 }
